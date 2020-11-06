87607507888ec08f3639b9899525af717bf08559
marioacar <mcarvantes@ucol.mx>
2020-11-06 13:08:59 -0600

    1!

--- jS/inventario.js
+++ jS/inventario.js
@@ -12,3 +12,17 @@ BuscarPorC()
 RecuperarAllP()
 RecuperarInvertidoP()
 IndicarPosicionInsertar()
+import Productos from "./js/productos.js"
+export default class Inventario{
+
+    constructor(){
+        this.Lista = []  
+    }
+}
+
+AgregarNuevoP()
+EliminarPorC()
+BuscarPorC()
+RecuperarAllP()
+RecuperarInvertidoP()
+IndicarPosicionInsertar()
