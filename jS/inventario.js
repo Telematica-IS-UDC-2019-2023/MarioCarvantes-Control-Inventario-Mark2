8e4ec7e8c61cf5e1b0e0578d4294c996590565d3
marioacar <mcarvantes@ucol.mx>
2020-11-07 12:32:47 -0600

    buscar y Ãlistar

--- jS/inventario.js
+++ jS/inventario.js
@@ -3,32 +3,60 @@ export default class Inventario{
     constructor(){
         this.inicio = null  
     }
-}
 
-AgregarNuevoP(nuevo){
-    if(this.inicio === null){
-        this.inicio = nuevo
-    }else{
-        let t = this.inicio
-        while(t.next != null){
-            t.next = nuevo
+    AgregarNuevoP(nuevo){
+        if(this.inicio === null){
+            this.inicio = nuevo
+        }else{
+            let t = this.inicio
+            while(t.next != null){
+                t.next = nuevo
+            }
         }
     }
-}
-
-EliminarPorC(codi){
-    let t = this.inicio
-    if(codi === inicio){
-        this.inico = t.next.next
-    } else{
-        while(t.next.codigo != codi){
+    
+    EliminarPorC(codi){
+        let t = this.inicio
+        if(codi === inicio){
+            this.inicio = t.next.next
+        } else{
+            while(t.next.codigo != codi){
+                t = t.next
+            }
+        }
+    }
+    
+    BuscarPorC(codi){
+        if(t.codigo === codi){
+            return t.nombre
+        }
+        let t = this.inicio
+        while(t.codigo != codi){
             t = t.next
         }
     }
+    //AKA LISTAR
+    RecuperarAllP(){
+        let all = ""
+        let t = this.inicio
+        while (t != null){
+            all += t.nombre + ""
+            t = t.next
+        }
+        return all
+    }
+    //AKA LISTA INVERTIDA
+    RecuperarInvertidoP(){
+        let t = t.inicio
+        let t = this.inicio
+        while(t != null){
+            all += t.nombre + ""
+            t = t.next
+        }
+        return all
+    }
+    
+    IndicarPosicionInsertar()
+    
 }
 
-
-BuscarPorC()
-RecuperarAllP()
-RecuperarInvertidoP()
-IndicarPosicionInsertar()
